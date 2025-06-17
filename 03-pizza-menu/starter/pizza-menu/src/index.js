159489f898ed92e57a16442cb9fb02aa6c925f3b
Herve Desrosiers <hdesrosiers@stingray.com>
2025-06-17 13:22:36 -0400

    39. JavaScript Logic in Components

--- 03-pizza-menu/starter/pizza-menu/src/index.js
+++ 03-pizza-menu/starter/pizza-menu/src/index.js
@@ -71,10 +71,14 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 10;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
   return (
-    <footer>{new Date().toLocaleTimeString()}. We're currently open!</footer>
+    <footer>{isOpen ? "We're currently open!" : "Sorry, we're closed."}</footer>
   );
-  //   return React.createElement("footer", null, "We're currently open!");
 }
 
 function Pizza() {
