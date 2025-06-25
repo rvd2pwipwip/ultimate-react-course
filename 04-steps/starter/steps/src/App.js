f0c8a4e8f10583ee6329e7f9543fa85d42d37126
Herve Desrosiers <hdesrosiers@stingray.com>
2025-06-25 09:42:34 -0400

    67. A Vanilla JavaScript Implementation

--- 04-steps/starter/steps/src/App.js
+++ 04-steps/starter/steps/src/App.js
@@ -7,6 +7,15 @@ const messages = [
 ];
 
 export default function App() {
+  return (
+    <>
+      <Steps />
+      <Steps />
+    </>
+  );
+}
+
+function Steps() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
 
@@ -23,7 +32,7 @@ export default function App() {
   };
 
   return (
-    <>
+    <div>
       <button className="close" onClick={handleClose}>
         &times;
       </button>
@@ -55,6 +64,6 @@ export default function App() {
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 }
