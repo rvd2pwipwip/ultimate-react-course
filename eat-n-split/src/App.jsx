44d3280635206c3e5857b61b8aefca04416a0414
Herve Desrosiers <hdesrosiers@stingray.com>
2025-08-14 08:21:38 -0400

    98. Displaying the New Friend Form

--- eat-n-split/src/App.jsx
+++ eat-n-split/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -19,12 +21,30 @@ const initialFriends = [
   },
 ];
 
+const Button = ({ children, onClick }) => {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
 function App() {
+  const [showFriendForm, setShowFriendForm] = useState(false);
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        {showFriendForm && <FormAddFriend />}
+        <Button
+          onClick={() => {
+            setShowFriendForm((show) => !show);
+          }}
+        >
+          {showFriendForm ? "Close" : "Add Friend"}
+        </Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -57,9 +77,50 @@ const Friend = ({ friend }) => {
           </p>
         )}
         {!friend.balance && <p>You and {friend.name} are even</p>}
-        <button className="button">Select</button>
+        <Button>Select</Button>
       </li>
     </div>
+  );
+};
+
+const FormAddFriend = () => {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+
+  const handleClick = () => {};
+
+  return (
+    <form className="form-add-friend">
+      <>
+        <label>👥 Friend name</label>
+        <input type="text" onChange={(e) => setName(e.value)} />
+        <label>🌄 Image URL</label>
+        <input type="text" onChange={(e) => setImage(e.value)} />
+      </>
+
+      <Button onClick={handleClick}>Add</Button>
+    </form>
+  );
+};
+
+const FormSplitBill = ({ friend }) => {
+  const handleClick = () => {};
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with {friend}</h2>
+      <label>💰 Bill value</label>
+      <input type="text" />
+      <label>🫵 Your Expense</label>
+      <input type="text" />
+      <label>🙂 {friend}'s Expense</label>
+      <input type="text" disabled />
+      <label>💰 Who's paying?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">{friend}</option>
+      </select>
+      <Button onClick={handleClick}>Split Bill</Button>
+    </form>
   );
 };
 
